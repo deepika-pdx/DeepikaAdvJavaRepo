ee0958917ad3e7be2578436094dd19273c9bc68e
Deepika Velapure <velapure@pdx.edu>
2023-01-22 22:33:18 -0800

    Commit test cases added for testing command line arguments parsing.

--- airline/src/it/java/edu/pdx/cs410J/velapure/Project1IT.java
+++ airline/src/it/java/edu/pdx/cs410J/velapure/Project1IT.java
@@ -15,16 +15,99 @@ class Project1IT extends InvokeMainTestCase {
      * Invokes the main method of {@link Project1} with the given arguments.
      */
     private MainMethodResult invokeMain(String... args) {
-        return invokeMain( Project1.class, args );
+        return invokeMain(Project1.class, args);
     }
 
-  /**
-   * Tests that invoking the main method with no arguments issues an error
-   */
-  @Test
-  void testNoCommandLineArguments() {
-    MainMethodResult result = invokeMain();
-    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
-  }
+    /**
+     * Tests that invoking the main method with no arguments issues an error
+     */
+    @Test
+    void testNoCommandLineArguments() {
+        MainMethodResult result = invokeMain();
+        assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+    }
+
+    /**
+     * Tests that invoking the main method with less than eight arguments issues an error
+     * and provides user with necessary information
+     */
+    @Test
+    void testTooFewCommandLineArguments() {
+        MainMethodResult result = invokeMain("IndiGo", "789", "PUN");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Too few command line arguments"));
+    }
+
+    /**
+     * Tests that invoking the main method with more than ten arguments (including optional arguments) issues an error
+     * and provides user with necessary information
+     */
+    @Test
+    void testTooManyCommandLineArguments() {
+        MainMethodResult result = invokeMain("IndiGo", "789", "PUN", "01/22/2023", "14:30", "HYD", "01/22/2023", "16:30", "KOL", "586", "-print");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Too many command line arguments"));
+    }
+
+    /**
+     * Tests that providing flight number with value that is not numeric issues an error
+     */
+    @Test
+    void testFlightNumberContainsOnlyNumbers() {
+        MainMethodResult result = invokeMain("IndiGo", "789c", "PUN", "01/22/2023", "14:30", "HYD", "01/22/2023", "16:30");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Flight number provided should consist of only numbers between 0-9."));
+    }
+
+    /**
+     * Tests that providing source location with value that is not alphabetic (a-zA-Z) or contains alphabets more or less than 3 issues an error
+     */
+    @Test
+    void testSourceLocationContainsOnlyThreeAlphabets() {
+        MainMethodResult result = invokeMain("IndiGo", "789", "PUN1#", "01/22/2023", "14:30", "HYD", "01/22/2023", "16:30");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Source location provided should consist of only three alphabets [a-zA-Z]"));
+    }
+
+    /**
+     * Tests that providing departure date in format other than mm/dd/yyyy issues an error
+     */
+    @Test
+    void testDepartureDateIsProvidedInCorrectFormat() {
+        MainMethodResult result = invokeMain("IndiGo", "789", "PUN", "15/12/2023", "14:30", "HYD", "01/22/2023", "16:30");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Departure date provided should be in mm/dd/yyyy format."));
+    }
+
+    /**
+     * Tests that providing departure time in format other than 24-hour time hh:mm issues an error
+     */
+    @Test
+    void testDepartureTimeIsProvidedInCorrectFormat() {
+        MainMethodResult result = invokeMain("IndiGo", "789", "PUN", "1/12/2023", "23:60", "HYD", "01/22/2023", "16:30");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Departure time provided should be in 24-hour(hh:mm) format."));
+    }
+
+    /**
+     * Tests that providing destination location with value that is not alphabetic (a-zA-Z) or contains alphabets more or less than 3 issues an error
+     */
+    @Test
+    void testDestinationLocationContainsOnlyThreeAlphabets() {
+        MainMethodResult result = invokeMain("IndiGo", "789", "PUN", "01/22/2023", "14:30", "HYDBD", "01/22/2023", "16:30");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Destination location provided should consist of only three alphabets [a-zA-Z]"));
+    }
+
+    /**
+     * Tests that providing arrival date in format other than mm/dd/yyyy issues an error
+     */
+    @Test
+    void testArrivalDateIsProvidedInCorrectFormat() {
+        MainMethodResult result = invokeMain("IndiGo", "789", "PUN", "1/3/2023", "14:30", "HYD", "1/22/23", "16:30");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Arrival date provided should be in mm/dd/yyyy format."));
+    }
+
+    /**
+     * Tests that providing arrival time in format other than 24-hour time hh:mm issues an error
+     */
+    @Test
+    void testArrivalTimeIsProvidedInCorrectFormat() {
+        MainMethodResult result = invokeMain("IndiGo", "789", "PUN", "1/12/2023", "23:59", "HYD", "01/22/2023", "24:00");
+        assertThat(result.getTextWrittenToStandardError(), containsString("Arrival time provided should be in 24-hour(hh:mm) format."));
+    }
 
 }
--- airline/src/main/java/edu/pdx/cs410J/velapure/Project1.java
+++ airline/src/main/java/edu/pdx/cs410J/velapure/Project1.java
@@ -2,22 +2,118 @@ package edu.pdx.cs410J.velapure;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
+
 /**
  * The main class for the CS410J airline Project
  */
 public class Project1 {
 
-  @VisibleForTesting
-  static boolean isValidDateAndTime(String dateAndTime) {
-    return true;
-  }
-
-  public static void main(String[] args) {
-    Flight flight = new Flight();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-    System.err.println("Missing command line arguments");
-    for (String arg : args) {
-      System.out.println(arg);
+    @VisibleForTesting
+    static boolean isValidDateAndTime(String dateAndTime) {
+        return true;
     }
-  }
 
+    public static void main(String[] args) {
+        if (args != null) {
+            int argLength = args.length;
+            if (argLength == 0) {
+                System.err.println("Missing command line arguments. Please enter arguments in the order: " +
+                        "<airline_name>, <flight_number>, <source_location>, <departure_date>, <departure_time>, " +
+                        "<destination_location, <arrival_date>, <arrival_time>, '-print' (optional), '-README' (optional).");
+            } else if (argLength < 8) {
+                System.err.println("Too few command line arguments. Please enter all the non-optional arguments in the order: " +
+                        "<airline_name>, <flight_number>, <source_location>, <departure_date>, <departure_time>, " +
+                        "<destination_location, <arrival_date>, <arrival_time>, '-print' (optional), '-README' (optional).");
+                System.err.println("Arguments provided: ");
+                for (String arg : args) {
+                    System.out.println(arg);
+                }
+            } else if (argLength > 10) {
+                System.err.println("Too many command line arguments. Please enter correct number of arguments " +
+                        "(optional and/or non-optional) in the order: " +
+                        "<airline_name>, <flight_number>, <source_location>, <departure_date>, <departure_time>, " +
+                        "<destination_location, <arrival_date>, <arrival_time>, '-print' (optional), '-README' (optional).");
+                System.err.println("Arguments provided: ");
+                for (String arg : args) {
+                    System.out.println(arg);
+                }
+            } else {
+                String airlineName = args[0];
+
+                String flightNumberString = args[1];
+                int flightNumber = 0;
+                try {
+                    flightNumber = Integer.parseInt(flightNumberString);
+                } catch (NumberFormatException e) {
+                    System.err.println("Flight number provided should consist of only numbers between 0-9.");
+                }
+
+                String srcLocation = args[2];
+                if (!(Pattern.matches("[a-zA-Z]+", srcLocation)) || srcLocation.length() != 3) {
+                    System.err.println("Source location provided should consist of only three alphabets [a-zA-Z].");
+                }
+
+                String departureDateString = args[3];
+                validateDate(departureDateString, "Departure");
+
+                String departureTime = args[4];
+                validateTime(departureTime, "Departure");
+
+                String destLocation = args[5];
+                if (!(Pattern.matches("[a-zA-Z]+", destLocation)) || destLocation.length() != 3) {
+                    System.err.println("Destination location provided should consist of only three alphabets [a-zA-Z].");
+                }
+
+                String arrivalDate = args[6];
+                validateDate(arrivalDate, "Arrival");
+
+                String arrivalTime = args[7];
+                validateTime(arrivalTime, "Arrival");
+            }
+        }
+
+        Flight flight = new Flight();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+
+
+    }
+
+    private static void validateDate(String providedDateString, String travelDateType) {
+        try {
+            SimpleDateFormat dateFormatterOne = new SimpleDateFormat("MM/dd/yyyy");
+            Date formattedDateTypeOne = dateFormatterOne.parse(providedDateString);
+            SimpleDateFormat dateFormatterTwo = new SimpleDateFormat("M/dd/yyyy");
+            Date formattedDateTypeTwo = dateFormatterTwo.parse(providedDateString);
+            SimpleDateFormat dateFormatterThree = new SimpleDateFormat("MM/d/yyyy");
+            Date formattedDateTypeThree = dateFormatterThree.parse(providedDateString);
+            SimpleDateFormat dateFormatterFour = new SimpleDateFormat("M/d/yyyy");
+            Date formattedDateTypeFour = dateFormatterThree.parse(providedDateString);
+            if (!(dateFormatterOne.format(formattedDateTypeOne).equals(providedDateString) || dateFormatterTwo.format(formattedDateTypeTwo).equals(providedDateString) || dateFormatterThree.format(formattedDateTypeThree).equals(providedDateString) || dateFormatterFour.format(formattedDateTypeFour).equals(providedDateString))) {
+                System.err.println(travelDateType + " date provided should be in mm/dd/yyyy format.");
+            }
+        } catch (ParseException e) {
+            System.err.println(travelDateType + " date provided should be in mm/dd/yyyy format.");
+        }
+    }
+
+    private static void validateTime(String providedTime, String travelTimeType) {
+        String[] providedTimeArray = providedTime.split(":");
+        if (providedTimeArray.length == 2) {
+            try {
+                int providedHour = Integer.parseInt(providedTimeArray[0]);
+                int providedMinute = Integer.parseInt(providedTimeArray[1]);
+                if (!(providedHour > 0 && providedHour <= 23 && providedMinute > 0 && providedMinute <= 59)) {
+                    System.err.println(travelTimeType + " time provided should be in 24-hour(hh:mm) format.");
+                }
+
+            } catch (NumberFormatException e) {
+                System.err.println(travelTimeType + " time provided should be in 24-hour(hh:mm) format.");
+            }
+        } else {
+            System.err.println(travelTimeType + " time provided should be in 24-hour(hh:mm) format.");
+        }
+    }
 }
