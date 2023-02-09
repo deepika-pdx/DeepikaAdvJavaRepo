a300e91a0a80f1af7fce99762db4bd6e37ee49b7
Deepika Velapure <velapure@pdx.edu>
2023-02-08 16:01:39 -0800

    Commit fix for Project3Test for Project 3.

--- airline/src/test/java/edu/pdx/cs410J/velapure/Project3Test.java
+++ airline/src/test/java/edu/pdx/cs410J/velapure/Project3Test.java
@@ -25,7 +25,7 @@ class Project3Test {
             assertThat(readme, not(nullValue()));
             BufferedReader reader = new BufferedReader(new InputStreamReader(readme));
             String line = reader.readLine();
-            assertThat(line, containsString("Assignment Name: CS510 Advance Java Project 2"));
+            assertThat(line, containsString("Assignment Name: CS510 Advance Java Project"));
         }
     }
 }
