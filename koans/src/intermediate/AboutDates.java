baa4fcfea89abeed029d5a8b54509629baf30cbf
Deepika Velapure <velapure@pdx.edu>
2023-02-11 21:16:34 -0800

    Commit fix date koan.

--- koans/src/intermediate/AboutDates.java
+++ koans/src/intermediate/AboutDates.java
@@ -46,7 +46,7 @@ public class AboutDates {
     @Koan
     public void usingDateFormatToFormatDate() {
         String formattedDate = DateFormat.getDateInstance().format(date);
-        assertEquals(formattedDate, "03-Mar-1973");
+        assertEquals(formattedDate, "Mar 3, 1973");
     }
 
     @Koan
