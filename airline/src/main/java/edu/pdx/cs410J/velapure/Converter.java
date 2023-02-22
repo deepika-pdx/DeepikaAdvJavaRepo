4c9bf56b2f94f177b06eaae087df709ce86f5b0c
Deepika Velapure <velapure@pdx.edu>
2023-02-22 09:49:25 -0800

    Commit changes for javadocs.

--- airline/src/main/java/edu/pdx/cs410J/velapure/Converter.java
+++ airline/src/main/java/edu/pdx/cs410J/velapure/Converter.java
@@ -6,6 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * This is <code>Converter</code> class for Project.
+ */
 public class Converter {
 
     /**
